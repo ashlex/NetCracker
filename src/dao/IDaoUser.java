f746f03619633669e510ce387f55df747c78e4b1
ashlex <angeldust.ru@gmail.com>
2014-11-15 01:28:51 +0200

    настройка проекта

--- src/dao/IDaoUser.java
+++ src/dao/IDaoUser.java
@@ -1,7 +1,5 @@
 package dao;
 
-/**
- * Created by Alexej on 15.11.2014.
- */
+
 public interface IDaoUser {
 }
--- src/entity/User.java
+++ src/entity/User.java
@@ -1,8 +1,5 @@
 package entity;
 
-/**
- * Created by Alexej on 15.11.2014.
- */
 public class User {
 	private String login=null;
 	private String login1=null;

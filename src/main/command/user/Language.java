160205a4dafa79a62547498997d7a283460f38b2
ashlex <angeldust.ru@gmail.com>
2014-12-19 11:31:43 +0300

    Попытка сделать смену языка

---
+++ src/main/command/user/Language.java
@@ -0,0 +1,47 @@
+package main.command.user;
+
+import java.io.IOException;
+import java.util.Locale;
+
+public class Language extends AbstractCommandOnUser {
+
+	public Language(){
+		this("locale");
+	}
+
+	public Language(String alias) {
+		super(alias);
+	}
+
+	@Override
+	public boolean execute() throws IOException {
+		Locale locale=getLocale();
+		if(locale!=null) {
+			context.setLocale(locale);
+			return true;
+		}
+		return false;
+	}
+
+	private Locale getLocale(){
+		switch (attributes.size()){
+			case 1:
+				if(attributes.get(0).matches("([a-zA-Z]{2,8})[_-]([a-zA-Z]{2}|[0-9]{3})")){
+					String str[]=attributes.get(0).split("[_-]]");
+					return new Locale(str[0],str[1]);
+				}else if(attributes.get(0).matches("([a-zA-Z]{2,8})")){
+					return new Locale(attributes.get(0));
+				}
+				return null;
+			case 2:
+				if(attributes.get(0).matches("[a-zA-Z]{2,8}")) {
+					if (attributes.get(1).matches("[a-zA-Z]{2}|[0-9]{3}")) {
+						return new Locale(attributes.get(0), attributes.get(1));
+					}
+				}
+				return null;
+			default:
+				return null;
+		}
+	}
+}
